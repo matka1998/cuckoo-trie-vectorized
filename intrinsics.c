e19e903f4f82b28852117a06f831ba963cc2d712
Liron-Lev <lironlev2604@gmail.com>
2025-09-27 17:11:40 +0300

    Change _mm_load_epi32 to _mm_load_si128 to support Adams machine :)

--- intrinsics.c
+++ intrinsics.c
@@ -3,17 +3,15 @@
 
 
 inline __m128i load_all_common_headers_in_bucket(ct_bucket* bucket) {
-
     uint8_t headers[CUCKOO_BUCKET_SIZE*4] = { 0 };
     for (int i = 0; i < CUCKOO_BUCKET_SIZE; i++) {
         for (int j = 0; j < 3; j++) {
-            headers[i * 4 + j] = __atomic_load_n((uint8_t*)&bucket->cells[i], __ATOMIC_ACQUIRE);
+            headers[i * 4 + j] = __atomic_load_n((uint8_t*)&bucket->cells[i] + j, __ATOMIC_ACQUIRE);
         }
     }
-    // 3 bytes for each common header, 4 headers per bucket totals 12 bytes, or 1 qword + 1 dword.
 
     // Then read and expand.
-    return _mm_load_epi32(headers);
+    return _mm_load_si128((const __m128i*)headers);
 }
 
 inline __m256i load_both_bucket_common_headers(ct_bucket * primary_bucket, ct_bucket * secondary_bucket) {
