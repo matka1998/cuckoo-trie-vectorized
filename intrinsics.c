b0e2a2b033e3b81beb10a1df8635a5b446896564
Liron-Lev <lironlev2604@gmail.com>
2025-09-29 10:55:50 +0300

    Improve running time for ycsb-a from 3900~ to 2870~

--- intrinsics.c
+++ intrinsics.c
@@ -2,38 +2,51 @@
 #include <immintrin.h>
 
 
-inline __m128i load_all_common_headers_in_bucket(ct_bucket* bucket) {
-    uint8_t headers[CUCKOO_BUCKET_SIZE*4] = { 0 };
-    for (int i = 0; i < CUCKOO_BUCKET_SIZE; i++) {
-        for (int j = 0; j < 3; j++) {
-            headers[i * 4 + j] = __atomic_load_n((uint8_t*)&bucket->cells[i] + j, __ATOMIC_ACQUIRE);
-        }
-    }
+// Assumes CUCKOO_BUCKET_SIZE == 4
+static inline __m256i load_both_bucket_common_headers(ct_bucket* primary_bucket,
+                                                      ct_bucket* secondary_bucket) {
+    assert(CUCKOO_BUCKET_SIZE == 4);
+    // Prefetch buckets to reduce latency
+    _mm_prefetch((const char*)primary_bucket, _MM_HINT_T0);
+    _mm_prefetch((const char*)secondary_bucket, _MM_HINT_T0);
 
-    // Then read and expand.
-    return _mm_load_si128((const __m128i*)headers);
+    // Load 8 dwords (4 from each bucket) with acquire semantics
+    const uint32_t p0 = __atomic_load_n((const uint32_t*)&primary_bucket->cells[0], __ATOMIC_RELAXED);
+    const uint32_t p1 = __atomic_load_n((const uint32_t*)&primary_bucket->cells[1], __ATOMIC_RELAXED);
+    const uint32_t p2 = __atomic_load_n((const uint32_t*)&primary_bucket->cells[2], __ATOMIC_RELAXED);
+    const uint32_t p3 = __atomic_load_n((const uint32_t*)&primary_bucket->cells[3], __ATOMIC_RELAXED);
+
+    const uint32_t s0 = __atomic_load_n((const uint32_t*)&secondary_bucket->cells[0], __ATOMIC_RELAXED);
+    const uint32_t s1 = __atomic_load_n((const uint32_t*)&secondary_bucket->cells[1], __ATOMIC_RELAXED);
+    const uint32_t s2 = __atomic_load_n((const uint32_t*)&secondary_bucket->cells[2], __ATOMIC_RELAXED);
+    const uint32_t s3 = __atomic_load_n((const uint32_t*)&secondary_bucket->cells[3], __ATOMIC_RELAXED);
+
+    const __m128i primary_values_vec = _mm_setr_epi32((int)p0, (int)p1, (int)p2, (int)p3);
+    const __m128i secondary_values_vec = _mm_setr_epi32((int)s0, (int)s1, (int)s2, (int)s3);
+
+    return _mm256_inserti128_si256(_mm256_castsi128_si256(primary_values_vec),
+                                   secondary_values_vec,
+                                   1);
 }
 
-inline __m256i load_both_bucket_common_headers(ct_bucket * primary_bucket, ct_bucket * secondary_bucket) {
-    __m128i primary_data = load_all_common_headers_in_bucket(primary_bucket);
-    __m128i secondary_data = load_all_common_headers_in_bucket(secondary_bucket);
-
-    return _mm256_setr_m128i(primary_data, secondary_data);
-}
-
-int find_free_cell_by_mask(ct_bucket* primary_bucket, ct_bucket* secondary_bucket, uint32_t mask, uint32_t primary_values, uint32_t secondary_values) {
+int find_free_cell_by_mask(ct_bucket* primary_bucket, ct_bucket* secondary_bucket,
+                           uint32_t mask, uint32_t primary_values, uint32_t secondary_values) {
     __m256i headers = load_both_bucket_common_headers(primary_bucket, secondary_bucket);
 
-    // TODO: Make sure in disassembly that we do in fact output vpbroadcastd and not an instruction sequence.
-    __m256i mask_vec = _mm256_set1_epi32(mask);
+    // Apply the 24-bit header constraint once together with the provided mask
+    const uint32_t combined_mask = mask & 0x00FFFFFFu;
+    __m256i mask_vec = _mm256_set1_epi32((int)combined_mask);
 
-    // TODO: Try to alternatively call set1 for the 256 bit register variation and combine using blend. Blend is more efficient that way,
-    // but the set is worse for 256 bit - try to experiment what works better.
-    __m128i primary_values_vec = _mm_set1_epi32(primary_values);
-    __m128i secondary_values_vec = _mm_set1_epi32(secondary_values);
-    __m256i combined_values_vec = _mm256_setr_m128i(primary_values_vec, secondary_values_vec);
+    __m128i primary_values_vec = _mm_set1_epi32((int)primary_values);
+    __m128i secondary_values_vec = _mm_set1_epi32((int)secondary_values);
+    __m256i combined_values_vec = _mm256_inserti128_si256(
+                                      _mm256_castsi128_si256(primary_values_vec),
+                                      secondary_values_vec,
+                                      1);
 
-    __mmask8 result = _mm256_cmp_epi32_mask(_mm256_and_si256(headers, mask_vec), combined_values_vec, _MM_CMPINT_EQ);
+    __mmask8 result = _mm256_cmp_epi32_mask(_mm256_and_si256(headers, mask_vec),
+                                            combined_values_vec,
+                                            _MM_CMPINT_EQ);
 
     if (result == 0)
         return -1;
